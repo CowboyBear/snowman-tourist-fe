86f76aa081ec5cb0317cfda081c93d7566011f95
Matheus Jatczak Bertazzo <matheus.j.bertazzo@gmail.com>
2020-05-27 19:59:56 -0300

    location picture implemented to being handled properly

--- src/app/components/locations-page/locations-page.component.ts
+++ src/app/components/locations-page/locations-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationService } from 'src/app/services/location.service';
 import { Location } from '../../models/Location';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-locations-page',
@@ -11,12 +12,19 @@ export class LocationsPageComponent implements OnInit {
 
   public locations: Location[];
 
+  private readonly UPLOADED_FILES_DIR = environment.apiUrl + '/uploadedfiles/';
+
   constructor(private locationService: LocationService) { }
 
   ngOnInit() {
     this.locationService.get().subscribe((locations: Location[]) => {
       this.locations = locations;
-    });
+    });    
   }
 
+  public getLocationPicturePath(location: Location): string {
+    return location.picturePath != null ? 
+      this.UPLOADED_FILES_DIR + location.picturePath : 
+      '/assets/img/default-location-img.png'
+  }
 }
